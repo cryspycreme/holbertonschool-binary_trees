b66caf87eb630b2c5ac59a1bbb239dd1d32b7de7
“cryspycreme” <11529@holbertonstudents.com>
2025-08-04 04:44:01 +0000

    Update parent == NULL condition

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -14,6 +14,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node = binary_tree_node(parent, value);
 	
+	/*if parent == NULL, set new node = parent*/
 	if (parent == NULL)
 	{
 		parent = new_node;
