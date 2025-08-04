a2faf2436f840df2841ccb0f5badebdc85637d8b
“cryspycreme” <11529@holbertonstudents.com>
2025-08-04 04:01:20 +0000

    Fix betty styles

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -7,18 +7,18 @@
 * @parent: pointer to the parent node of the node to create
 * @value: the value to input into the node
 * Return: pointer to new node, or NULL on failure.
-*/ 
+*/
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *current;
-	
+
 	/*allocate memory for the node and check for failure*/
 	current = malloc(sizeof(binary_tree_t));
-	
+
 	if (current == NULL)
-        	return (NULL);
-	
+		return (NULL);
+
 	current->n = value;
 	current->parent = parent;
 	current->left = NULL;
