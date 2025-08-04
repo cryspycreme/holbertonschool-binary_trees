412d39557b7ae6ca8c39029ae63ea6ad5b6ad414
“cryspycreme” <11529@holbertonstudents.com>
2025-08-04 04:50:03 +0000

    Add null condition for new node

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -16,10 +16,10 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	
 	/*if parent == NULL, set new node = parent*/
 	if (parent == NULL)
-	{
-		parent = new_node;
-                return (new_node);
-	}
+                return (NULL);
+
+	if (new_node == NULL)
+		return (NULL);
 	
 	if (parent->left != NULL)
 	{
