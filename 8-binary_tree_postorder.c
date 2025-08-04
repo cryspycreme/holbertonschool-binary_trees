d8a3dad00ef8957db79fdb1934cb94aa26c37694
“cryspycreme” <11529@holbertonstudents.com>
2025-08-04 08:40:42 +0000

    Fix betty styles

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -14,7 +14,7 @@ void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree == NULL || func == NULL)
 		return;
-	
+
 	binary_tree_postorder(tree->left, func); /*recur on left node*/
 	binary_tree_postorder(tree->right, func); /*recur on the right node*/
 	func(tree->n); /*Deal with the current node*/
