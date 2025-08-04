7caa8b4a57c5d2bc51ed8e9f55f5103d18444376
“cryspycreme” <11529@holbertonstudents.com>
2025-08-04 05:04:20 +0000

    Trying to fix bug

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -14,9 +14,15 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node = binary_tree_node(parent, value);
 	
-	/*if parent == NULL, set new node = parent*/
-	if (parent == NULL)
+	if (new_node == NULL)
                 return (NULL);
+
+	/*if parent == NULL, return null*/
+	if (parent == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
 
 	if (new_node == NULL)
 		return (NULL);
