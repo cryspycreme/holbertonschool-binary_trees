7e0b4c2b31504e917f4644f4b932410a55a63f3c
“cryspycreme” <11529@holbertonstudents.com>
2025-08-04 05:15:51 +0000

    Fix betty styles

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -14,25 +14,25 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node = binary_tree_node(parent, value);
 
-        if (new_node == NULL) /*CHECK 1: check this first*/
-                return (NULL);
+	if (new_node == NULL) /*CHECK 1: check this first*/
+		return (NULL);
 
-        /*if parent == NULL, return null*/
-        if (parent == NULL)
-        {
-                free(new_node);
-                return (NULL);
-        }
+	/*if parent == NULL, return null*/
+	if (parent == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
 
-        if (new_node == NULL)
-                return (NULL);
+	if (new_node == NULL)
+		return (NULL);
 
-        if (parent->right != NULL)
-        {
-                new_node->right = parent->right;
-                new_node->right->parent = new_node; /*Update old right child's parent*/
-        }
-        parent->right = new_node;
+	if (parent->right != NULL)
+	{
+		new_node->right = parent->right;
+		new_node->right->parent = new_node; /*Update old right child's parent*/
+	}
+	parent->right = new_node;
 
-        return (new_node);
+	return (new_node);
 }
