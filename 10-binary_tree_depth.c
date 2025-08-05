42b14d69cc21bbdfc0c6bc64fa516255d146d0b4
“cryspycreme” <11529@holbertonstudents.com>
2025-08-05 05:38:53 +0000

    Fix betty styles

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -5,17 +5,17 @@
 /**
 * binary_tree_depth - measures the depth of a binary tree
 *
-* @tree: a pointer to the target node. 
+* @tree: a pointer to the target node.
 * Return: depth of tree (count).
 */
 
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
 	size_t depth = 0;
-	
+
 	if (tree == NULL)
 		return (0);
-		
+
 	/*traverse through the nodes*/
 	while (tree->parent != NULL)
 	{
