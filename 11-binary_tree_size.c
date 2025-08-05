f9b86c206bea4c3c266c4e731e3ae5879c1ff069
“cryspycreme” <11529@holbertonstudents.com>
2025-08-05 06:09:28 +0000

    Draft code for 11-tree_size

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+* binary_tree_size - measures the size of a binary tree
+*
+* @tree: a pointer to the target node. 
+* Return: size of tree (count).
+*/
+
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t left;
+	size_t right;
+	
+	if (tree == NULL)
+		return (0);
+	
+	/*if tree is a leaf node*/
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+
+	left = binary_tree_size(tree->left);
+	right = binary_tree_size(tree->right);
+	return (left + right);
+}
