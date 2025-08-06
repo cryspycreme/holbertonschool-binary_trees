502cfb4e33c0dfa01c68113cb4c2ac72cfdfc8cb
“cryspycreme” <11529@holbertonstudents.com>
2025-08-06 02:59:45 +0000

    Fix betty styles

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -13,10 +13,10 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 {
 	size_t left;
 	size_t right;
-	
+
 	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
 		return (0);
-	
+
 	left = binary_tree_nodes(tree->left);
 	right = binary_tree_nodes(tree->right);
 	return (left + right + 1);
