11c48d07528f4e20fddf08e89ca1c31f742f7fc5
“cryspycreme” <11529@holbertonstudents.com>
2025-08-06 06:41:57 +0000

    Update single node condition

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -36,16 +36,19 @@ size_t binary_tree_height(const binary_tree_t *tree)
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
 	int left;
-  int right;
+	int right;
+	
+	if (tree == NULL)
+		return (0);
 
-  if (tree == NULL)
-	  return (0);
-	      
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+
 	left = binary_tree_height(tree->left);
-  right = binary_tree_height(tree->right);
-  
-  if ((left == right) && (tree->left != NULL && tree->right != NULL))
-	   return (1);
-	 else
-		 return (0);
+	right = binary_tree_height(tree->right);
+	
+	if ((left == right) && (tree->left != NULL && tree->right != NULL))
+		return (1);
+	else
+		return (0);
 }
