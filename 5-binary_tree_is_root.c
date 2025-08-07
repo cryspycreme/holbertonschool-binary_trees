30815e1cd56530da5bef7fddfb87c6078adaac0a
“cryspycreme” <11529@holbertonstudents.com>
2025-08-07 03:51:45 +0000

    Update code

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -11,9 +11,10 @@
 
 int binary_tree_is_root(const binary_tree_t *node)
 {
+	if (node == NULL)
+		return (0);
+	
 	if (node->parent == NULL)
 		return (1);
-	else if (node == NULL)
-		return (0);
 	else return (0);
 }
