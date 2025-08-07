6c2393dc9490ca37a12c55619a17ddd6b9166600
“cryspycreme” <11529@holbertonstudents.com>
2025-08-07 03:24:26 +0000

    Update null condition

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -11,11 +11,14 @@
 
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	binary_tree_t *parent = node->parent;
-	binary_tree_t *grandparent = node->parent->parent;
+	binary_tree_t *parent;
+	binary_tree_t *grandparent;
 
-	if (node == NULL || parent == NULL || grandparent == NULL)
+	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
 		return (NULL);
+
+	*parent = node->parent;
+        binary_tree_t *grandparent = node->parent->parent;
 
 	if (parent == grandparent->left && grandparent->right != NULL)
 		return (grandparent->right);
